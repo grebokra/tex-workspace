aff4a4f0b3c2d3c158ba843a20cd5512f53b4762
sadautomata <levi.kaiser@yandex.ru>
2021-09-06 00:19:11 +0300

    : Added scripts

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+	echo 'Error: you should provide a main file in tex format as an argument. :(' 1>&2
+	exit 2
+fi
+
+if [ ! -f $1 ]; then
+	echo 'Error: file does not exist' 1>&2
+	exit 3
+fi
+
+docker run -ti \
+  -v miktex:/miktex/.miktex \
+  -v `pwd`:/miktex/work \
+  miktex/miktex \
+  pdflatex $1
